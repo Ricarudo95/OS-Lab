dd3962f904aaadfe3b93bb2bbb6bbca72384c261
Your Name <you@example.com>
2019-02-14 19:40:26 -0400

    lab 3 fix

--- Lab 3/shell.c
+++ Lab 3/shell.c
@@ -1,6 +1,6 @@
-#include <pthread.h>
+#include <sys/types.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
 
@@ -15,13 +15,16 @@ int main(int argc, char * argv[]) {
             exit(0);
         }
         else if(strcmp(argv[0], "help") == 0){
-            ...
+            exit(0);
+        }
         }
         else if(strcmp(argv[0], "history") == 0){
-            ...
+            exit(0);
+        }
         }
         else if(strcmp(argv[0], "cd") == 0){
-            ...
+            exit(0);
+        }
         }
         else{
             execute(argv);
@@ -30,5 +33,24 @@ int main(int argc, char * argv[]) {
 }
 
 void execute(argv){
+    pid_t pid;
+    /* fork a child process */
+    pid = fork();
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, “Fork Failed”);
+        exit(-1);
+    }
+
+    else if (pid == 0) { /* child process */
+        execlp(“/bin/ls”, “ls”, NULL);
+    }
+    else { 
+    /* parent process */
+    /* parent will wait for the child to complete */
+        wait(NULL);
+        printf(“Child Complete!\n”);
+        exit (0);
+    }
 
 }
